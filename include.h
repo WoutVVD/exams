557bcc576abc0797c5021eaea0642d357a39b08c
WoutVVD <wout.vanvoorden@gmail.com>
2024-12-05 14:53:52 +0100

    small updates

---
+++ include.h
@@ -0,0 +1,7 @@
+#define ADDRESS     "tcp://192.168.0.106:1883"                  // !! IP-address:Port-ID
+#define CLIENTID    "RaspberryPiClient"                         // Unique client ID
+#define SUB_TOPIC   "ExamWout"                                  // MQTT topic
+#define QOS         1                                           // Quality of Service level
+#define TIMEOUT     10000L                                      // Timeout in milliseconds
+
+#define filepath    "./logging.log"                             //path to log file
