35849eeca0383336cea1687b6258f76aeb9a83a0
WoutVVD <wout.vanvoorden@gmail.com>
2024-12-06 10:10:55 +0100

    name change + tbl const + tbl inserts + header printer

---
+++ exam.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <MQTTClient.h>
+#include "include.h"            //file with #includes
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+//structure of tbl
+struct tbl
+{
+    char datum_tijd_stroom[DATE_TIME_LEN]; 
+    int tarief_indicator;
+    float actueel_stroomverbruik;
+    float actueel_spanning;
+    float totaal_dagverbruik;
+    float totaal_nachtverbruik;
+    float totaal_dagopbrengst;
+    float totaal_nachtopbrengst;
+    char datum_tijd_gas[DATE_TIME_LEN];
+    float totaal_gasverbruik;
+    struct tbl *next;
+};
+struct tbl *head = NULL;
+struct tbl *current = NULL;
+
+//insert first
+void insert_first(struct tbl **head, char *datetime, int *rate_indicator, float *current_power, float *current_voltage, float *total_day_consum, float *total_night_consum, float *total_day_output, float *total_night_output, float *total_gas_consum)
+{
+    struct tbl *lk = (struct tbl *)malloc(sizeof(struct tbl));
+
+    strcpy(lk->datum_tijd_stroom, datetime);
+    strcpy(lk->tarief_indicator, rate_indicator);
+    strcpy(lk->actueel_stroomverbruik, current_power);
+    strcpy(lk->actueel_spanning, current_voltage);
+    strcpy(lk->totaal_dagverbruik, total_day_consum);
+    strcpy(lk->totaal_nachtverbruik, total_night_consum);
+    strcpy(lk->totaal_dagopbrengst, total_day_output);
+    strcpy(lk->totaal_nachtopbrengst, total_night_output);
+    strcpy(lk->datum_tijd_gas, datetime);
+    strcpy(lk->totaal_gasverbruik, total_gas_consum);
+
+    lk->next = NULL;
+
+    *head = lk;
+
+}
+
+void insert_first(struct tbl *list, char *datetime, int *rate_indicator, float *current_power, float *current_voltage, float *total_day_consum, float *total_night_consum, float *total_day_output, float *total_night_output, float *total_gas_consum)
+{
+    struct tbl *lk = (struct tbl *)malloc(sizeof(struct tbl));
+
+    strcpy(lk->datum_tijd_stroom, datetime);
+    strcpy(lk->tarief_indicator, rate_indicator);
+    strcpy(lk->actueel_stroomverbruik, current_power);
+    strcpy(lk->actueel_spanning, current_voltage);
+    strcpy(lk->totaal_dagverbruik, total_day_consum);
+    strcpy(lk->totaal_nachtverbruik, total_night_consum);
+    strcpy(lk->totaal_dagopbrengst, total_day_output);
+    strcpy(lk->totaal_nachtopbrengst, total_night_output);
+    strcpy(lk->datum_tijd_gas, datetime);
+    strcpy(lk->totaal_gasverbruik, total_gas_consum);
+
+    lk->next = NULL;
+
+    list->next = lk;
+}
+
+// This function is called upon when a message is delivered
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+    #ifdef DEBUG
+        printf("Message with token value %d delivery confirmed\n", dt);
+        printf( "-----------------------------------------------\n" );
+    #endif    
+    deliveredtoken = dt;
+}
+
+// This function is called upon when an incoming message from mqtt is arrived
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    char *error_in = message->payload;
+
+    printf( "Msg in : <%s>\n", error_in );
+
+    // Create a new client to publish the message
+    MQTTClient client = (MQTTClient)context;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    
+    return 1;
+}
+
+//connection lost
+void connlost(void *context, char *cause) {
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
+
+//print
+void print_all(struct tbl **list){
+    struct tbl *temp = head;
+    
+    //print header
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+            "Elektriciteit- en gas verbruik - totalen per dag\n"
+            "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n"   //extra empty line
+            "STARTWAARDEN\n\n"                                                                              //extra empty line
+            "DATUM - TIJD:  %s\n"
+            "DAG \t Totaal verbruik \t = %f kWh\n"
+            "DAG \t Totaal opbrengst \t = %f kWh\n"
+            "NACHT \t Totaal verbruik \t = %f kWh\n"
+            "NACHT \t Totaal opbrengst \t = %f kWh\n"
+            "GAS \t Totaal verbruik \t = %f m3\n"
+            "------------------------------------------------------------------------------------------\n"
+            "TOTALEN\n"
+            "------------------------------------------------------------------------------------------\n\n" //extra empty line
+            ,temp->datum_tijd_stroom,temp->totaal_dagverbruik,temp->totaal_dagopbrengst,temp->totaal_nachtverbruik,temp->totaal_nachtopbrengst,temp->totaal_gasverbruik
+            );
+}
+
+
+int main() {
+   // Open MQTT client for listening
+    
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    MQTTClient_setCallbacks(client, client, connlost, msgarrvd, delivered);
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    MQTTClient_subscribe(client, SUB_TOPIC, QOS);
+
+    // Keep the program running to continue receiving and publishing messages
+    for(;;) {
+        delay(1);
+    }
+
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
--- include.h
+++ include.h
@@ -4,5 +4,7 @@
 #define QOS         1                                           // Quality of Service level
 #define TIMEOUT     10000L                                      // Timeout in milliseconds
 
+#define DATE_TIME_LEN  17                                       //length of date time string
+
 #define logFilepath    "./logging.log"                           //path to log file
 #define valuesFilepath "./values.txt"                            //path to values file
